bbdc8775e5e7df9193a33026d228ffd9c4a5fd04
oliRenan <oliveirarenan479@gmail.com>
2026-01-19 16:08:22 -0300

    error handling

--- src/queryOptions/useProductsQuery.ts
+++ src/queryOptions/useProductsQuery.ts
@@ -3,6 +3,20 @@ import type { Product } from "../types/productType";
 
 const getProducts = async ():Promise<Product[]> => {
     const response = await fetch(import.meta.env.VITE_API_URL);
+
+    if(!response.ok){
+        let errorMessage = `Erro ${response.status}: ${response.statusText}`;
+
+        try {
+            const errorBody = await response.json();
+            if (errorBody && errorBody.message) {
+                errorMessage = errorBody.message;
+            }
+        } catch {
+        }
+
+        throw new Error(errorMessage);
+    }
     return response.json();
 }
 
